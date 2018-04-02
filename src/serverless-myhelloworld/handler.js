d51301e86b3d61e393e507e7d8e79f4c5ebeb6a4
hpang123 <hantong.pang@hbo.com>
2018-04-02 19:12:07 -0400

    serverless framework

---
+++ src/serverless-myhelloworld/handler.js
@@ -0,0 +1,19 @@
+'use strict';
+/*
+ * To test:
+ * https://xxxxx.execute-api.us-east-2.amazonaws.com/dev/hello-world
+ */
+module.exports.helloWorld = (event, context, callback) => {
+  const response = {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*', // Required for CORS support to work
+    },
+    body: JSON.stringify({
+      message: 'Go Serverless v1.0! Your function executed successfully!',
+      input: event,
+    }),
+  };
+
+  callback(null, response);
+};
